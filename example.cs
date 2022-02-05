ea604fa5b9300ec90fc0ffedfaf886bafc34da6f
Gabriel Anderson <gabe4k@gmail.com>
2022-02-05 09:08:09 -0800

    adds keywords to syntax grammar

---
+++ example.cs
@@ -0,0 +1,15 @@
+// This is a comment
+let
+  id = (x) => x;
+  constant = (a, b) => a;
+  createFoo = (x) => {
+    name: ["F", "o", "o", "b", "a", "r"],
+    value: x
+  };
+  bar = 999;
+in do {
+  baz = id(bar);
+  constant(baz, bar);
+  clientName = createFoo(baz).name;
+  return clientName;
+}
